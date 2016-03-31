b0c99a45a57438baa59a6547a5a379f80dcdf94a
vinik <vinicius.kirst@gmail.com>
2016-03-30 21:57:06 -0300

    tests

---
+++ chef/cookbooks/ckrst/spec/default_spec.rb
@@ -0,0 +1,24 @@
+require 'chefspec'
+
+at_exit { ChefSpec::Coverage.report! }
+
+describe 'ckrst::default' do
+    let (:chef_run) { ChefSpec::SoloRunner.converge(described_recipe) }
+
+    it 'creates ckrst group' do
+        chef_run.converge(described_recipe)
+        expect(chef_run).to create_group('ckrst')
+    end
+
+    it 'creates vinicius group' do
+        chef_run.converge(described_recipe)
+        expect(chef_run).to create_group('vinicius')
+    end
+
+    it 'creates vinicius user' do
+        chef_run.converge(described_recipe)
+        expect(chef_run).to create_user('vinicius')
+    end
+
+
+end
