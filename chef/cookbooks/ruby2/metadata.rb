d02538bae93b341f7238e57fa922edd739cae89b
Vinícius Kirst <vinicius@versul.com.br>
2016-08-08 18:16:37 -0300

    cookbook extracted

--- chef/cookbooks/ruby2/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'ruby2'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures ruby2'
-long_description 'Installs/Configures ruby2'
-version          '0.1.0'
-
-depends 'apt'
